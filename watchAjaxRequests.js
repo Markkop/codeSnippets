beb36f816df336df3037da658ad9fe7f9490ab7b
Markkop <marcelo.kopmann@linx3.com>
2020-02-06 10:40:17 -0300

    feat: XHR http request intercept

--- watchAjaxRequests.js
+++ watchAjaxRequests.js
@@ -72,3 +72,53 @@ function watchAjaxRequests() {
   const documentJQueryElement = window.$(document)
   documentJQueryElement.off('ajaxComplete', doSomething).on('ajaxComplete', doSomething)
 }
+
+/**
+ * OPTION 2: Without JQuery
+ */
+
+ /**
+ * Intercepts a XHR HTTP Request and executes a function
+ * @param { Function } XHR window.XMLHttpRequest
+ * @param { Function } postCompleteFunction
+ */
+export function interceptRequest (XHR, postCompleteFunction) {
+  'use strict'
+  var open = XHR.prototype.open
+  var send = XHR.prototype.send
+
+  XHR.prototype.open = function (method, url, async, user, pass) {
+    this._url = url
+    open.call(this, method, url, async, user, pass)
+  }
+
+  XHR.prototype.send = function (data) {
+    var self = this
+    var oldOnReadyStateChange
+    var url = this._url
+
+    function onReadyStateChange () {
+      if (self.readyState === 4 /* complete */) {
+        /* This is where you can put code that you want to execute post-complete */
+        /* URL is kept in this._url */
+        postCompleteFunction(url)
+      }
+
+      if (oldOnReadyStateChange) {
+        oldOnReadyStateChange()
+      }
+    }
+
+    /* Set xhr.noIntercept to true to disable the interceptor for a particular call */
+    if (!this.noIntercept) {
+      if (this.addEventListener) {
+        this.addEventListener('readystatechange', onReadyStateChange, false)
+      } else {
+        oldOnReadyStateChange = this.onreadystatechange
+        this.onreadystatechange = onReadyStateChange
+      }
+    }
+
+    send.call(this, data)
+  }
+}
